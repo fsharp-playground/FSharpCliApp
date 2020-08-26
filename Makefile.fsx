06e78aaaa1b8c2adcb6be2852e7c605a623af4c9
Abhinav Sharma <abhi18av@gmail.com>
2020-08-26 23:44:06 +0530

    add the base commands to Makefile.fsx

---
+++ Makefile.fsx
@@ -0,0 +1,32 @@
+#r "paket:
+nuget Fake.Core.Target //"
+//#load "./.fake/build.fsx/intellisense.fsx"
+// include Fake modules, see Fake modules section
+
+// NOTE Use VsCode
+
+open Fake.Core
+
+// *** Define Targets ***
+Target.create "Clean" (fun _ ->
+  Trace.log " --- Cleaning stuff --- "
+)
+
+Target.create "Build" (fun _ ->
+  Trace.log " --- Building the app --- "
+)
+
+Target.create "Deploy" (fun _ ->
+  Trace.log " --- Deploying app --- "
+)
+
+
+open Fake.Core.TargetOperators
+
+// *** Define Dependencies ***
+"Clean"
+  ==> "Build"
+  ==> "Deploy"
+
+// *** Start Build ***
+Target.runOrDefault "Deploy"
